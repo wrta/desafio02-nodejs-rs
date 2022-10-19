9f73fb1db464614fe683e793421932b0e878c33b
wrta <gustavom.cupini@gmail.com>
2022-10-19 15:04:16 -0300

    faltam 6 testes a passar

--- src/index.js
+++ src/index.js
@@ -20,61 +20,59 @@ function checksExistsUserAccount(request, response, next) {
 
 
   request.user = user;
-  return next();
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   const {user} = request;
-  
 
-  if(user.pro === false && user.todos.length >= 10){
+  if((user.pro === false) && (user.todos.length >= 10)){
     return response.status(403).json({error: "You're not a Pro User and have reached your limit of todos"})
   }
-
-  
-
-  return next();
+  next();
 }
 
 function checksTodoExists(request, response, next) {
   const {username} = request.headers;
-  const {id} = request.params;
+  const {id: todoId} = request.params;
+
+  if(!validate(todoId)){
+    return response.status(400).json({error: 'Invalid Todo Format!'})
+  }
 
   const user = users.find(user => {user.username === username});
 
   if(!user){
     return response.status(404).json({error: "User not found!"});
   }
+  
+  
 
-  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  const checkTodo = user.todos.find((todo) => todo.id === todoId);
 
-  const checkTodo = user.todos.find(todo => todo.id === id);
 
   if(!checkTodo){
-    return response.status(400).json({error: "Todo Not Found"});
+    return response.status(404).json({error: "Todo Not Found"});
   }
-
-  const checkTodoID = regexExp.test(checkTodo.id);
-
-  if(!checkTodoID){
-    return response.status(400).json({error: "The Todo ID is not a UUID"});
-  }
-
-  request.todo = todo;
+  
   request.user = user;
-  return next();
+  request.todo = todo;
+  
+  next();
 }
 
 function findUserById(request, response, next) {
-  const {id} = request.params;
-  const user = users.find(user => user.id === id);
+  const {id: todoId} = request.params;
+
+  const user = users.find(user => user.id === todoId);
 
   if(!user){
     return response.status(404).json({error: 'User not found by ID'});
   }
 
+
   request.user = user;
-  return next();
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -109,7 +107,7 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
   const { user } = request;
 
   if (user.pro) {
-    return response.status(404).json({ error: 'Pro plan is already activated.' });
+    return response.status(400).json({ error: 'Pro plan is already activated.' });
   }
 
   user.pro = true;
@@ -164,7 +162,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
   const todoIndex = user.todos.indexOf(todo);
 
   if (todoIndex === -1) {
-    return response.status(204).json({ error: 'Todo not found' });
+    return response.status(404).json({ error: 'Todo not found' });
   }
 
   user.todos.splice(todoIndex, 1);
